3a782f0c9cf13c46ba16a9199d52470cdd4d908d
Renato Groffe <renato.groffe@yahoo.com.br>
2021-01-14 10:16:39 -0300

    Testes
    Testes

---
+++ TesteSelenium/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+
+namespace TesteSelenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testando 123..");
+
+            var driver = new RemoteWebDriver(new Uri("http://127.0.0.1:4444/wd/hub"), new FirefoxOptions());
+
+
+            var siteTestes = Environment.GetEnvironmentVariable("SiteTestes");
+            driver.Navigate().GoToUrl("http://www.google.com/");
+
+            System.Threading.Thread.Sleep(3000);
+
+            Console.WriteLine($"Resultado: {driver.Title}");
+
+            //Close the browser
+            driver.Quit();
+        }
+    }
+}
